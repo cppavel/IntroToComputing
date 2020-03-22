887c6dcefff3519d119a0cf2ff2dfd031a2f891f
Pavel Petrukhin <petrukhp@tcd.ie>
2020-03-22 01:40:28 +0000

    support for denormalized values

--- lab3-fpadd.s
+++ lab3-fpadd.s
@@ -23,8 +23,9 @@ FP_A	EQU	0x00000000
 	BL fpexp
 	MOV R2, R0
 	
-	MOV R0, R1
-	MOV R1, R2
+	LDR R0, =0x00080000
+	LDR R1, =-0x0000007F
+	;MOV R1, R2
 	
 	BL fpencode
 
@@ -91,7 +92,7 @@ fpexp
 ;
 fpencode
 	
-	PUSH {lr, R4-R7}
+	PUSH {lr, R4-R9}
 	
 	MOV R4, #0 ; sign flag
 	CMP R0, #0 ; if(fraction is negative)
@@ -99,18 +100,51 @@ fpencode
 	NEG R0, R0
 	MOV R4, #1 ; negative
 notif
-	LDR R5, =0x00800000 ;mask for setting the leading 1
+	LDR R5, =0x80000000 ;mask for finding the leading one
+	LDR R8, = 31; leading one position
+while
+	AND R9, R0, R5 ;getting the current bit
+	CMP R9, #0 ;while(bit==0&&mask!=0)
+	BNE endwhile
+	CMP R5, #0
+	BEQ endwhile
+	MOV R5, R5, LSR #1; moving the mask
+	SUB R8, R8, #1; moving the counter	
+	B while
+endwhile
+
+	LDR R5, =23; the index where leading 1 resides, when value is normalized
+	SUB R8, R8, R5; offset
+	CMP R8, #0; if(offset is negative)
+	BGE notif0
+	NEG R9, R8 ;converting offset to be positive
+	MOV R0, R0, LSL R9 ;shifting left instead of right
+	B endelse0
+notif0
+	MOV R0, R0, LSR R8 ;moving the value so as to make it normalized
+endelse0	
+
+	LDR R5, =0x00800000 ;mask for clearing the leading 1
 	BIC R0, R0, R5; clearing the leading 1	
 	MOV R4, R4, LSL #31; moving the sign bit so we can set it
 	ORR R0, R0, R4; setting the sign bit
 	
 	LDR R6, =127; exponent bias
 	ADD R1, R1, R6; getting the exponent into its usual form
+	ADD R1, R1, R8; adding the offset to exponent
+	CMP R1, #255 ;if(exponent>128)
+	BLE notif1
+	MOV R1, #255; setting the exponent to be 128 if it turns out to be greater than it.
+notif1
+	CMP R1, #0  ;if(exponent < -127)
+	BGE notif2
+	MOV R1, #0 ;setting the exponent to be -127
+notif2
 	MOV R1, R1, LSL #23; shifting the exponent to the left
-	LDR R7, =0x7F800000;  mask for getting the exponent
+	LDR R7, =0x7F800000;  mask for clearing the place for the exponent
 	BIC R0, R0, R7; clearing the bits where the exponent should be
 	ORR R0, R0, R1; copying the mask to the return register
 	
-	POP {pc, R4-R7}
+	POP {pc, R4-R9}
 	
 	END
